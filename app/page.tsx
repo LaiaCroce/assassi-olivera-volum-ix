baa38e1b0ee0787606bb031d71c85df9e496f716
lcroce28 <lcj10@alumnes.udl.cat>
2026-06-01 20:19:34 +0200

    ara?

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
             </div>
 
             <img
-              src="/collaboradors/el-rebost-de-casa.png"
+              src="public/collaboradors/el-rebost-de-casa.png"
               alt="El Rebost de Casa"
               className="max-h-16 max-w-full object-contain opacity-90"
             /> 
